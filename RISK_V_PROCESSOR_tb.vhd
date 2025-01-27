4315e421900902b4bcba83720e5080e84418dc87
Gleb <72437559+glebpav@users.noreply.github.com>
2025-01-27 13:11:05 +0300

    Update RISK_V_PROCESSOR_tb.vhd

--- RISK_V_PROCESSOR_tb.vhd
+++ RISK_V_PROCESSOR_tb.vhd
@@ -8,34 +8,34 @@ end entity;
 
 architecture tb of processor_tb is
 
-	signal clk            : STD_LOGIC := '0';
-    signal rst            : STD_LOGIC := '1';
+	signal clk            : STD_LOGIC;
+    signal rst            : STD_LOGIC;
 	
-	component processor 
+	component RISC_V_PROCESSOR 
 		port (
-			i_clk		: in STD_LOGIC;
-			i_rst		: in STD_LOGIC
+			i_clk					: in STD_LOGIC;
+			i_rst				  	: in STD_LOGIC
 		);
 	end component;
 	
 	component processor_tester
 		port (
-			o_clk		: out STD_LOGIC;
-			o_rst		: out STD_LOGIC
+			o_clk					: out STD_LOGIC;
+			o_rst					: out STD_LOGIC
 		);
 	end component;
 	
 	begin 
-		uut : processor
+		uut : RISC_V_PROCESSOR 
 		port map (
-			i_clk	=> clk,
-			i_rst	=> rst
+			i_clk					=> clk,
+			i_rst					=> rst
 		);
 		
-		tester : processor_tester
+		tester : processor_tester 
 		port map (
-			o_clk	=> clk,
-			o_rst	=> rst
+			o_clk					=> clk,
+			o_rst					=> rst
 		);
 	
 end architecture tb;
